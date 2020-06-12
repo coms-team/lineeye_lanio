c417300b5d6f082060ceb765e0907847378b4e47
Shohei Fujii <fujii.shohei@gmail.com>
2020-06-12 17:00:32 +0900

    wip

--- include/lineeye/lanio.h
+++ include/lineeye/lanio.h
@@ -5,11 +5,12 @@ extern "C" {
 #include "LELanio.h"
 }
 #include <sstream>
+#include <memory>
 
 namespace lineeye {
 
 class LanIOException : public std::runtime_error {
-    public:
+  public:
     LanIOException(const std::string& msg) : std::runtime_error(msg) {
         std::stringstream ss;
         ss << msg << " - ";
@@ -42,11 +43,25 @@ class LanIOException : public std::runtime_error {
     }
 };
 
+class LanIODevice {
+  public:
+    LanIODevice(const std::string& ipaddress);
+    virtual ~LanIODevice();
+    void set_output_port(int port, bool t);
+    void set_output_ports(int bit);
+    bool get_input_port(int port);
+    int get_input_ports();
+
+  protected:
+    int handle_;
+};
 class LanIO {
   public:
     LanIO();
     virtual ~LanIO();
     int get_num_devices();
     std::string get_ip_address(int lanio_num);
+    std::unique_ptr<LanIODevice> get_device(const std::string& ipaddress);
 };
+
 }  // namespace lineeye
--- src/lanio.cpp
+++ src/lanio.cpp
@@ -1,8 +1,7 @@
-#include <pybind11/pybind11.h>
+// #include <pybind11/pybind11.h>
+// namespace py = pybind11;
+#include "lineeye/lanio.h"
 #include <boost/format.hpp>
-
-namespace py = pybind11;
-#include "lanio.h"
 
 namespace lineeye {
 
@@ -29,14 +28,57 @@ std::string LanIO::get_ip_address(int i) {
     return std::move(std::string(ipaddress));
 }
 
+std::unique_ptr<LanIODevice> LanIO::get_device(const std::string& ipaddress) {
+    return std::make_unique<LanIODevice>(ipaddress);
+}
+LanIODevice::LanIODevice(const std::string& ipaddress) {
+    handle_ = LELanioConnectByIpAddress(const_cast<char*>(ipaddress.c_str()));
+    if (handle_ == -1) {
+        throw LanIOException((boost::format("could not connect to %1%") % ipaddress).str());
+    }
+}
+LanIODevice::~LanIODevice() {
+    LELanioClose(handle_);
+}
+
+void LanIODevice::set_output_port(int port, bool t) {
+    int ret = LELanioOutPio(handle_, port, static_cast<int>(t));
+    if (ret == 0) {
+        throw LanIOException((boost::format("failed to set output (port:%1%)") % port).str());
+    }
+}
+void LanIODevice::set_output_ports(int bit) {
+    int ret = LELanioOutPioAll(handle_, bit);
+    if (ret == 0) {
+        throw LanIOException((boost::format("failed to set output (bit:%0xd)") % bit).str());
+    }
+}
+
+bool LanIODevice::get_input_port(int port) {
+    int val;
+    int ret = LELanioInPio(handle_, port, &val);
+    if (ret == 0) {
+        throw LanIOException((boost::format("failed to get input (port:%1%)") % port).str());
+    }
+    return val;
+}
+int LanIODevice::get_input_ports() {
+    int bit;
+    int ret = LELanioInPioAll(handle_, &bit);
+    if (ret == 0) {
+        throw LanIOException((boost::format("failed to get inputs (bit:%0xd)") % bit).str());
+    }
+    return bit;
+}
+
 }  // namespace lineeye
 
 // clang-format off
-PYBIND11_MODULE(lineeye_lanio, m) {
-    py::class_<lineeye::LanIO>(m, "LanIO")
-        .def(py::init<>())
-        .def("get_num_devices", &lineeye::LanIO::get_num_devices)
-        .def("get_ip_address", &lineeye::LanIO::get_ip_address);
-    py::register_exception<lineeye::LanIOException>(m, "LanIOException");
-}
+// PYBIND11_MODULE(lineeye_lanio, m) {
+//     py::class_<lineeye::LanIO>(m, "LanIO")
+//         .def(py::init<>())
+//         .def("get_num_devices", &lineeye::LanIO::get_num_devices)
+//         .def("get_ip_address", &lineeye::LanIO::get_ip_address);
+//     py::register_exception<lineeye::LanIOException>(m, "LanIOException");
+// }
 // clang-format on
